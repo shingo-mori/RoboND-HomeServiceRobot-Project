0d6daaf01de1ea955ce78e752245288718ad25e2
shingo-uzuki <spawn.s.mori@gmail.com>
2018-06-10 21:51:40 +0900

    Add script for testing global localization

---
+++ catkin_ws/src/ShellScripts/test_navigation.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+catkin_ws_dir=/home/shingo/work/RoboND-HomeServiceRobot-Project/catkin_ws
+
+# deploy turtlebot in the project environment
+xterm -e roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=${catkin_ws_dir}/src/World/udacity.world &
+
+sleep 5
+
+# run amcl to perform global localization
+xterm -e roslaunch turtlebot_gazebo amcl_demo.launch map_file:=${catkin_ws_dir}/src/World/udacity.yaml &
+
+sleep 3
+
+# observe the navigation in Rviz
+xterm -hold -e roslaunch turtlebot_rviz_launchers view_navigation.launch
